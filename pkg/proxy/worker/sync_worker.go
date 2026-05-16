65e6227ac1616e328ff67cfa96b811cc991bb3b4
Bruce Arctor <5032356+brucearctor@users.noreply.github.com>
2026-05-16 15:38:08 -0700

    Revert "Update pkg/proxy/worker/sync_worker.go"
    This reverts commit 75ee68d49575470ce59b9cccbf70ab2306f3589c.

--- pkg/proxy/worker/sync_worker.go
+++ pkg/proxy/worker/sync_worker.go
@@ -92,7 +92,6 @@ func (w *SyncWorker) processBatch() {
 	var corruptedIDs []string
 
 	// 2. Deserialize and flag retries
-	var corruptedIDs []string
 	for _, obs := range outboxSpans {
 		var span storage.Span
 		if err := json.Unmarshal([]byte(obs.PayloadJSON), &span); err != nil {
@@ -100,13 +99,6 @@ func (w *SyncWorker) processBatch() {
 			corruptedIDs = append(corruptedIDs, obs.SpanID)
 			continue
 		}
-
-		// ... rest of the loop ...
-	}
-
-	if len(corruptedIDs) > 0 {
-		_ = w.store.DeleteOutboxSpans(ctx, corruptedIDs)
-	}
 
 		// Optimistic Ingestion with Pessimistic Retries pattern.
 		if obs.AttemptCount > 0 {
